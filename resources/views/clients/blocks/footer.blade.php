957e53bc28816cb221426920f6f60d5089f3b1ea
bluecrypt47 <lechuong09291999@gmail.com>
2022-07-11 23:20:22 +0700

    create interface

---
+++ resources/views/clients/blocks/footer.blade.php
@@ -0,0 +1,6 @@
+<hr>
+<footer>
+    <div class="container" >
+        <p class="text-center">Copyright &copy; {{date('Y')}} by BlueC</p>
+    </div>
+</footer>
---
+++ resources/views/clients/blocks/header.blade.php
@@ -0,0 +1,30 @@
+<header class="py-3 border shadow">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <a href="{{ route('home') }}">
+                    <h1>HHT</h1>
+                </a>
+            </div>
+            <div class="col-8 d-flex justify-content-end align-items-center">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a href="{{ route('home') }}" class="nav-link active" aria-current="page">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active" aria-current="page">Products</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active" aria-current="page">Contact</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active" aria-current="page">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
